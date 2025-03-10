51a866852938eb9316aca30b32fe418f52766911
Razoppi <razoppienzo@gmail.com>
2025-03-10 17:33:40 -0300

    Project created

---
+++ src/test/java/com/ProjetoWebServices/ProjetoWebServices/ProjetoWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetoWebServices.ProjetoWebServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
