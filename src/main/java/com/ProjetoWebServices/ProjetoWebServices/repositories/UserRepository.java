ffdd7749c348e3237e22c5ce445ebe0078a417af
Razoppi <razoppienzo@gmail.com>
2025-03-24 10:17:19 -0300

    Service layer, component registration

--- src/main/java/com/ProjetoWebServices/ProjetoWebServices/repositories/UserRepository.java
+++ src/main/java/com/ProjetoWebServices/ProjetoWebServices/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package com.ProjetoWebServices.ProjetoWebServices.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ProjetoWebServices.ProjetoWebServices.entities.User;
 
+@Repository // não precisa pois o JpaResository já está marcado como componente do Spring
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
