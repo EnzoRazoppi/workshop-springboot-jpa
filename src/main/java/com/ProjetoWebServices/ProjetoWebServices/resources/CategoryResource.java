bebb8d63922903e4dc4f3744c70e8d9ec3c0afdb
Razoppi <razoppienzo@gmail.com>
2025-04-07 13:34:25 -0300

    Category HTTP methods

--- src/main/java/com/ProjetoWebServices/ProjetoWebServices/resources/CategoryResource.java
+++ src/main/java/com/ProjetoWebServices/ProjetoWebServices/resources/CategoryResource.java
@@ -1,15 +1,22 @@
 package com.ProjetoWebServices.ProjetoWebServices.resources;
 
+import java.net.URI;
 import java.util.List; 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ProjetoWebServices.ProjetoWebServices.entities.Category;
+import com.ProjetoWebServices.ProjetoWebServices.entities.User;
 import com.ProjetoWebServices.ProjetoWebServices.services.CategoryService;
 
 
@@ -32,6 +39,26 @@ public class CategoryResource {
 	public ResponseEntity<Category> findById(@PathVariable Long id){
 		Category category = service.findById(id);
 		return ResponseEntity.ok().body(category);
+	}
+	@PostMapping
+	public ResponseEntity<Category> insert(@RequestBody Category obj){  //RequestBody é uma annotation para dizer que o objeto user vai chegar no formato de JSON, no body
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+				path("/{id}").buildAndExpand(obj.getId()).toUri();  // o URI é a localização do novo objeto que vai ser adcionado, é necessario para cabeçalho da reposta no postman
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	//noContent vai retorna uma reposta vázia que é código 204
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<Category> update(@PathVariable Long id, @RequestBody Category obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 	
 
--- src/main/java/com/ProjetoWebServices/ProjetoWebServices/services/CategoryService.java
+++ src/main/java/com/ProjetoWebServices/ProjetoWebServices/services/CategoryService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ProjetoWebServices.ProjetoWebServices.entities.Category;
+import com.ProjetoWebServices.ProjetoWebServices.entities.User;
 import com.ProjetoWebServices.ProjetoWebServices.repositories.CategoryRepository;
+import com.ProjetoWebServices.ProjetoWebServices.services.exceptions.DatabaseException;
+import com.ProjetoWebServices.ProjetoWebServices.services.exceptions.ResourceNotFoundException;
 
 
 
@@ -20,8 +24,40 @@ public class CategoryService {
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
+	
 	public Category findById(Long Id) {
-		Optional<Category> objOptional = repository.findById(Id);
-		return objOptional.get();
+		Optional<Category> obj = repository.findById(Id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(Id));
 	}
+	public Category insert(Category obj) {
+		return repository.save(obj);
+	}
+	public void delete(Long id) {
+		try{
+			if(repository.existsById(id)){
+				repository.deleteById(id);
+			}
+			else {
+				throw new ResourceNotFoundException(id);
+			}
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+	}
+	public Category update(Long id, Category obj) {
+		if(repository.existsById(id)){
+			Category entity = repository.getReferenceById(id); //não vai no banco de dados, ele retorna um objeto monitorado pelo jpa
+			updateData(entity, obj);
+			return repository.save(entity);
+		}
+		else {
+			throw new ResourceNotFoundException(id);
+		}
+	}
+	private void updateData(Category entity, Category obj) {
+		entity.setName(obj.getName());
+		
+	}
+	
 }
